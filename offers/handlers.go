b597f683e234428ca3e1ed9cd1c4e47e72833684
Javier Guajardo <javier.guajardoj@gmail.com>
2022-11-05 21:36:04 +0200

    Base code for FindOffer and CreateOffer

--- offers/handlers.go
+++ offers/handlers.go
@@ -1,14 +1,32 @@
 package offers
 
-import "github.com/labstack/echo/v4"
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Offer struct {
+	Id string `json:"id"`
+	Description string `json:"description"`
+	Store string `json:"store"`
+	MaxClaims int `json:"maxClaims"`
+	CreatedAt time.Time `json:"createdAt"`
+}
 
 func Get(c echo.Context) error {
 	return c.String(200, "GET OFFER")
 }
 
 func Post(c echo.Context) error {
-	return c.String(200, "CREATE OFFER")
-
+	var offer Offer
+	json.NewDecoder(c.Request().Body).Decode(&offer)
+	if CreateOffer(offer) {
+		return c.String(200, "Offer created successfully")
+	} else {
+		return c.String(400, "Error creating offer")
+	}
 }
 
 func Delete(c echo.Context) error {
